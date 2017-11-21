a5c4686c9af5c7eef8dc6be2e26d7b4bf779ac03
Jake Baumgartner <jabbaum@gmail.com>
2017-11-21 10:32:46 -0800

    Add e handler methods for changing term & location state of SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -11,7 +11,10 @@ class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {term: '', location: '', sortby: 'best_match'};
+    this.handleTermChange = this.handleTermChange.bind(this);//binding this to the eventhandler
+    this.handleLocationChange = this.handleLocationChange.bind(this);//binding this to the eventhandller
   }
+
   getSortByClass(sortByOption) {
     if (sortBy == sortByOption) {
       return 'active';
@@ -19,15 +22,26 @@ class SearchBar extends React.Component {
       return '';
     }
   }//end getSortByClass method
+
   handleSortByChange(sortByOption) {
     this.setState({sortBy: sortByOption});
   }// end event handler handleSortByChange
+
   renderSortByOptions() {
     return Object.keys(sortByOptions).map(sortByOption => {
       let sortByOptionValue = sortByOptions[sortByOption];
       return <li key={sortByOptionValue} className={this.getSortByClass(sortByOptionValue)} onClick={this.handleSortByChange(sortByOptionValue, this)}>{sortByOption}</li>
     });
   }//end renderSortByOptions method
+
+  handleTermChange(e) {
+    this.setState({term: e.target.value});
+  }//end event handler handleTermChange
+
+  handleLocationChange(e) {
+    this.setState({location: e.target.value});
+  }//end event handler handleTermChange
+
   render() {
     return (
       <div className="SearchBar">
