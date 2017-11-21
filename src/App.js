3f8f72042b980065111245237083a387b76d5feb
Jake Baumgartner <jabbaum@gmail.com>
2017-11-21 11:09:20 -0800

    Add searchYelp method and property in App to be sent to SearchBar.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
 
 class App extends Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }// end searchYelp method, params are the 3 pieces of info that will be sent to the yelp api
+
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
+        <SearchBar searchYelp={this.searchYelp} />
         <BusinessList />
       </div>
     );
