8f6ad810611d54e2689d513ffbc1903b4eb6fa56
Jake Baumgartner <jabbaum@gmail.com>
2017-11-28 18:44:10 -0800

    set up promise and fetch for yelp access token

--- src/util/yelp.js
+++ src/util/yelp.js
@@ -1,3 +1,11 @@
 const clientId = 'Q1q4K-fn2vUbxsbzLMssNg';
 const secret = 'T4tCNUe4zLXutdQ3yCeUHJZsk8oOqhdscC3QvkuZPeiuI1HrvxfeIiLtHeGgHAwt';
 let accessToken;
+
+let yelp = {getAccessToken() {
+  if (accessToken) {
+    return new Promise(resolve => resolve(accessToken));
+  }// we already have a token so we need a promise to resolve that token
+  return fetch(`https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`);
+}
+};
