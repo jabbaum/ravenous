6944089e5a37a4da90663ac06b5a98bcd1d7a2ec
Jake Baumgartner <jabbaum@gmail.com>
2017-11-28 18:52:20 -0800

    Add post request for getAccessToken.

--- src/util/yelp.js
+++ src/util/yelp.js
@@ -6,6 +6,10 @@ let yelp = {getAccessToken() {
   if (accessToken) {
     return new Promise(resolve => resolve(accessToken));
   }// we already have a token so we need a promise to resolve that token
-  return fetch(`https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, {method: 'POST'});
+  return fetch(`https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, {method: 'POST'}).then(response => {
+    return response.json();
+  }).then(jsonResponse => {
+    accessToken = jsonResponse.access_token;
+  });
 }
 };
